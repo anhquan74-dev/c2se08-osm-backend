94fcd19b13c3234815eda2dee9cd0d672ac8a9c5
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2023-04-05 18:32:22 +0700

    crud provider

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -201,4 +201,208 @@ class UserController extends Controller
     {
         dd(request('q'));
     }
+    // Get all provider
+    public function getAllProviders()
+    {
+        $providers = User::whereHas('user_role_details', function ($query) {
+            return $query->where('role_id', '=', 2);
+        })->get();
+        return response()->json([
+            'data' => $providers,
+            'statusCode' => 200,
+            'message' => 'Get all providers successful!',
+        ]);
+    }
+    // Get provider by Id
+    public function getProviderById(Request $request)
+    {
+        if ($request->id) {
+            $providerInfo = DB::table('users')
+                ->join('role_detail_users', 'users.id', '=', 'role_detail_users.user_id')
+                ->join('role_details', 'role_details.id', '=', 'role_detail_users.role_id')
+                ->where('users.id', $request->id)
+                ->get()->map(function ($providerInfo) {
+                    unset($providerInfo->user_id);
+                    unset($providerInfo->created_at);
+                    unset($providerInfo->updated_at);
+                    unset($providerInfo->email_verified_at);
+                    unset($providerInfo->remember_token);
+                    return $providerInfo;
+                });
+            if ($providerInfo->isEmpty()) {
+                return response()->json([
+                    'statusCode' => 404,
+                    'message' => 'Not found!',
+                ]);
+            }
+            return response()->json([
+                'data' => $providerInfo[0],
+                'statusCode' => 200,
+                'message' => 'Get provider info successfully!',
+            ]);
+        }
+        return response()->json([
+            'statusCode' => 400,
+            'message' => 'Missing provider id parameter!',
+        ]);
+    }
+    // Create a new provider
+    public function createNewProvider(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'full_name' => 'required|string|min:2|max:255',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $provider = User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'full_name' => $request->full_name,
+            'birthday' => $request->birthday,
+            'gender' => $request->gender,
+            'phone_number' => $request->phone_number,
+            'is_favorite' => false,
+            'is_working' => false,
+            'total_rate' => 0,
+            'total_star' => 0,
+            'avg_star' => 0,
+            'clicks' => 0,
+            'views' => 0,
+            'click_rate' => 0,
+            'is_valid_flag' => false,
+        ]);
+        RoleDetailUser::create([
+            'user_id' => $provider->id,
+            'role_id' => 3
+        ]);
+        return response()->json([
+            'data' => $provider,
+            'statusCode' => 201,
+            'message' => 'Successful created!',
+        ]);
+    }
+    // Update provider
+    public function updateProvider(Request $request)
+    {
+        if ($request->id) {
+            $providerUpdate = User::find($request->id);
+            if ($providerUpdate) {
+                if ($request->file('avatar') == null) {
+                    $validatorUpdate = Validator::make($request->all(), [
+                        'email' => 'string|email|max:255|unique:users',
+                        'full_name' => 'string|min:2|max:255',
+                    ]);
+                    if ($validatorUpdate->fails()) {
+                        return response()->json([
+                            "statusCode" => 400,
+                            "message" => "Validation update error",
+                            "errors" => $validatorUpdate->errors()
+                        ]);
+                    }
+                    $providerUpdate->email = $request->email;
+                    $providerUpdate->full_name = $request->full_name;
+                    $providerUpdate->birthday = $request->birthday;
+                    $providerUpdate->gender = $request->gender;
+                    $providerUpdate->phone_number = $request->phone_number;
+                    $providerUpdate->introduction = $request->introduction;
+                    $providerUpdate->is_favorite = $request->is_favorite;
+                    $providerUpdate->is_working = $request->is_working;
+                    $providerUpdate->total_rate = $request->total_rate;
+                    $providerUpdate->total_star = $request->total_star;
+                    $providerUpdate->avg_star = $request->avg_star;
+                    $providerUpdate->clicks = $request->clicks;
+                    $providerUpdate->views = $request->views;
+                    $providerUpdate->click_rate = $request->click_rate;
+                    $providerUpdate->is_valid_flag = $request->is_valid_flag;
+                    $providerUpdate->save();
+                    return response()->json([
+                        'statusCode' => 200,
+                        'message' => 'Customer updated successfully!',
+                    ]);
+                }
+                if ($request->hasFile('avatar')) {
+                    $validatorUpdate = Validator::make($request->all(), [
+                        'email' => 'string|email|max:255|unique:users',
+                        'full_name' => 'string|min:2|max:255',
+                        'avatar' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                    ]);
+                    if ($validatorUpdate->fails()) {
+                        return response()->json([
+                            "statusCode" => 400,
+                            "message" => "Validation update error",
+                            "errors" => $validatorUpdate->errors()
+                        ]);
+                    }
+                    $destination = 'uploads/avatar/' . $providerUpdate->avatar;
+                    if (File::exists($destination)) {
+                        File::delete($destination);
+                    }
+                    $image = $request->file('avatar');
+                    $fileName = Str::random(5) . date('YmdHis') . '.' . $image->getClientOriginalExtension();
+                    $image->move('uploads/avatar/', $fileName);
+                    $providerUpdate->email = $request->email;
+                    $providerUpdate->full_name = $request->full_name;
+                    $providerUpdate->birthday = $request->birthday;
+                    $providerUpdate->gender = $request->gender;
+                    $providerUpdate->phone_number = $request->phone_number;
+                    $providerUpdate->introduction = $request->introduction;
+                    $providerUpdate->is_favorite = $request->is_favorite;
+                    $providerUpdate->is_working = $request->is_working;
+                    $providerUpdate->total_rate = $request->total_rate;
+                    $providerUpdate->total_star = $request->total_star;
+                    $providerUpdate->avg_star = $request->avg_star;
+                    $providerUpdate->clicks = $request->clicks;
+                    $providerUpdate->views = $request->views;
+                    $providerUpdate->click_rate = $request->click_rate;
+                    $providerUpdate->is_valid_flag = $request->is_valid_flag;
+                    $providerUpdate->avatar = $fileName;
+                    $providerUpdate->save();
+                    return response()->json([
+                        'statusCode' => 200,
+                        'message' => 'Provider updated successfully!',
+                    ]);
+                }
+            } else {
+                return response()->json([
+                    "statusCode" => 404,
+                    "message" => "Can't find the provider you want to update!"
+                ]);
+            }
+        }
+        return response()->json([
+            'statusCode' => 400,
+            'message' => 'Missing provider id parameter!',
+        ]);
+    }
+    // Hard delete provider
+    public function hardDeleteProvider(Request $request)
+    {
+        if ($request->id) {
+            $checkProvider = User::where('id', $request->id)->first();
+            if ($checkProvider) {
+                User::where('id', $request->id)->delete();
+                return response()->json([
+                    'statusCode' => 200,
+                    'message' => 'Deleted provider successfully!',
+                ]);
+            } else {
+                return response()->json([
+                    "statusCode" => 404,
+                    "message" => "Can't find the provider you want to delete!"
+                ]);
+            }
+        }
+        return response()->json([
+            'statusCode' => 400,
+            'message' => 'Missing provider id parameter!',
+        ]);
+    }
+    // Searching, paginating and sorting providers
+    public function searchPaginationProviders(Request $request)
+    {
+        dd(request('q'));
+    }
 }
