e8fb6930368c5f357da53756f7b18d1c6789f2be
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2023-04-06 20:35:40 +0700

    crud appointment

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Appointment extends Model
@@ -16,7 +17,11 @@ class Appointment extends Model
     {
         return $this->hasOne(Feedback::class, 'appointment_id');
     }
-
+    // 1 - n with AttachPhoto Table
+    public function attachPhoto(): HasMany
+    {
+        return $this->hasMany(AttachPhoto::class, 'appointment_id');
+    }
     // Belong to Package Table
     public function package(): BelongsTo
     {
